bb215c3107079dd8545a4e9a43911b9325529cde
Shahnazafreen01 <shahnazafreen480@gmail.com>
2026-02-16 16:01:13 +0530

    Add files via upload

---
+++ c++/Assignment1(C++)/Part B/ass1 Part B Q11.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+
+int main(){
+    float m1, m2, m3, total_marks, percentage;
+
+    cout << "Enter marks of three subjects : ";
+    cin >> m1 >> m2 >> m3;
+
+    total_marks = m1 + m2 + m3;
+    percentage = (total_marks / 300) * 100;
+
+    cout << "Total = " << total_marks << endl;
+
+    cout << fixed << setprecision(2);
+    cout << "Percentage = " << percentage << "%";
+
+    return 0;
+
+}
